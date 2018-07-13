3adb19abaaa78d573dff3d16694ad1ebffac160c
Unknown <derik@oagro.com.br>
2018-07-13 06:10:53 -0300

    Adding dependencies, ireport and post.pdf

--- src/main/java/socialtravelling/controllers/PostController.java
+++ src/main/java/socialtravelling/controllers/PostController.java
@@ -1,11 +1,20 @@
 package socialtravelling.controllers;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import socialtravelling.models.Posts;
 import socialtravelling.repository.PostRepository;
 
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/posts")
@@ -14,6 +23,10 @@ public class PostController {
 
     @Autowired
     private PostRepository pr;
+
+    @Autowired
+    private ApplicationContext appContext;
+
 
     @PostMapping
     public Posts cadastrar(@RequestBody Posts posts) {
@@ -24,8 +37,6 @@ public class PostController {
     public List<Posts> buscarTodos(){
         return pr.findAll();
     }
-
-
 //    @DeleteMapping("/{id}")
 //    public void excluir(@PathVariable("id") Integer id) {
 //        pr.deleteById(id);
